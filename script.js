3864496872b5794ce7f575f8b0233a01f09ff9f3
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2022-10-26 12:03:41 +0300

    solution

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const firstWord = prompt("Inserisici una parola");
+const secondWord = prompt("Inserisci un'altra parola");
+
+if(firstWord.length === secondWord.length){
+    console.log(`Le parole sono della stessa lunghezza, non ve le stampo`);
+}else if(firstWord.length > secondWord.length){
+    console.log(firstWord, secondWord);
+}else{
+    console.log(secondWord, firstWord);
+}
